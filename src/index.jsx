17dd19b4918a9b8c1553872cd73deb97b2656744
a-amirfarzam <a.amirfarzam@gmail.com>
2022-04-16 23:43:45 +0430

    sass added

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import ReactDOM from 'react-dom'
 import Header from "./Header";
 import App from './App';
 import Footer from "./Footer";
-import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
-
 
 ReactDOM.render(
     <>
